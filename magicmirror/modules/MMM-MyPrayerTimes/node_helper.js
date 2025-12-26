a364d81cb43ef625bc4fa22f1155acd24ec06b33
hyoni <hyoni@example.com>
2025-12-25 20:15:14 -0600

    Fix linting in MMM-MyPrayerTimes node_helper.js

--- magicmirror/modules/MMM-MyPrayerTimes/node_helper.js
+++ magicmirror/modules/MMM-MyPrayerTimes/node_helper.js
@@ -11,43 +11,45 @@ MIT License
 //-------------------------------------------
 */
 
-const NodeHelper = require("node_helper");
 const https = require("https");
+const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-  start() {
-    console.log(`Starting node_helper for: ${this.name}`);
-  },
+	start() {
+		console.log(`Starting node_helper for: ${this.name}`);
+	},
 
-  getMPT(url) {
-    https.get(url, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-      res.on("end", () => {
-        try {
-          const result = JSON.parse(data);
-          if (result && result.data && result.data.timings) {
-            this.sendSocketNotification("MPT_RESULT", {
-              timings: result.data.timings,
-              hijri: result.data.date && result.data.date.hijri ? result.data.date.hijri : null
-            });
-          } else {
-            console.error("MMM-MyPrayerTimes: Invalid data format received");
-          }
-        } catch (error) {
-          console.error("MMM-MyPrayerTimes: Error parsing JSON", error);
-        }
-      });
-    }).on("error", (error) => {
-      console.error("MMM-MyPrayerTimes: Error fetching data", error);
-    });
-  },
+	getMPT(url) {
+		https
+			.get(url, (res) => {
+				let data = "";
+				res.on("data", (chunk) => {
+					data += chunk;
+				});
+				res.on("end", () => {
+					try {
+						const result = JSON.parse(data);
+						if (result && result.data && result.data.timings) {
+							this.sendSocketNotification("MPT_RESULT", {
+								timings: result.data.timings,
+								hijri: result.data.date && result.data.date.hijri ? result.data.date.hijri : null
+							});
+						} else {
+							console.error("MMM-MyPrayerTimes: Invalid data format received");
+						}
+					} catch (error) {
+						console.error("MMM-MyPrayerTimes: Error parsing JSON", error);
+					}
+				});
+			})
+			.on("error", (error) => {
+				console.error("MMM-MyPrayerTimes: Error fetching data", error);
+			});
+	},
 
-  socketNotificationReceived(notification, payload) {
-    if (notification === "GET_MPT") {
-      this.getMPT(payload);
-    }
-  }
+	socketNotificationReceived(notification, payload) {
+		if (notification === "GET_MPT") {
+			this.getMPT(payload);
+		}
+	}
 });
